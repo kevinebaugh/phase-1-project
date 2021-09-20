b47065a801e9b393c47f6896ca0ddb6577d55347
Kevin Ebaugh <kevinebaugh@gmail.com>
2021-09-20 11:17:14 -0400

    Add grid, styles, populate meters

--- index.js
+++ index.js
@@ -10,17 +10,19 @@ console.log("Hello, get ready to Teleport! 👍😎👍")
 // When one is clicked, fetch details and display them
 
 const getUrbanAreas = function() {
+  document.querySelector("#teleport").style.display = "none"
   const urbanAreas = []
   fetch("https://api.teleport.org/api/urban_areas/")
   .then(response => response.json())
   .then(data => {
     data["_links"]["ua:item"].forEach(element => {
-      const ua = document.createElement("li")
+      const ua = document.createElement("div")
       ua.innerText = element.name
       ua.className = "urban-area"
       let slug = element.href.split("/")[5].slice(5)
       ua.id = slug
       ua.addEventListener("click", handleUrbanAreaClick)
+      ua.addEventListener("dblclick", handleUrbanAreaDoubleClick)
       document.querySelector("#urban-areas").appendChild(ua)
     })
     return urbanAreas
@@ -29,22 +31,33 @@ const getUrbanAreas = function() {
 
 document.querySelector("#teleport").addEventListener("click", getUrbanAreas)
 
+const showcase = document.querySelector("#showcase")
+
 const handleUrbanAreaClick = function(event) {
+  showcase.style.display = "inherit"
   const name = document.querySelector("#name")
-  const internet = document.querySelector("#internet")
-  const water = document.querySelector("#water")
   const taxi = document.querySelector("#taxi")
-  const guns = document.querySelector("#guns")
+  const internetMeter = document.querySelector("#internet-meter")
+  const waterMeter = document.querySelector("#water-meter")
+  const gunsMeter = document.querySelector("#guns-meter")
   name.innerText = event.srcElement.innerText
   const slug = event.target.id
-  console.log(`Urban area ${slug} has been clicked!`)
+
   fetch(`https://api.teleport.org/api/urban_areas/slug%3A${slug}/details/`)
   .then(response => response.json())
   .then(data => {
-    console.log(data)
-    internet.innerText = "populated in function"
-    water.innerText = "populated in function"
-    taxi.innerText = "populated in function"
-    guns.innerText = "populated in function"
+    const internetData = data.categories.find(e => e.id == "NETWORK").data.find(e => e.id == "NETWORK-DOWNLOAD-TELESCORE").float_value
+    const waterData = data.categories.find(e => e.id == "POLLUTION").data.find(e => e.id == "DRINKING-WATER-QUALITY-TELESCORE").float_value
+    const taxiData = data.categories.find(e => e.id == "COST-OF-LIVING").data.find(e => e.id == "COST-TAXI").currency_dollar_value.toFixed(2)
+    const gunsData = data.categories.find(e => e.id == "SAFETY").data.find(e => e.id == "GUN-SCORE-TELESCORE").float_value
+    internetMeter.value = internetData
+    waterMeter.value = waterData
+    gunsMeter.value = gunsData
+    taxi.innerText = `$${taxiData}`
   })
+}
+
+const handleUrbanAreaDoubleClick = function(event) {
+  const targetDiv = document.querySelector(`#${event.target.id}`)
+  targetDiv.style.display = "none"
 }
